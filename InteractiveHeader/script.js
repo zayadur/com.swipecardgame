9f517f757b2c76dbc0587d010b85a0e5d591d601
zayadur <zayadur@outlook.com>
2022-04-23 13:45:27 -0400

    Display cards, clean up styles, start buttons

--- InteractiveHeader/script.js
+++ InteractiveHeader/script.js
@@ -1,24 +1,16 @@
 var cards = [
-	"Card1",
-	"Card2",
-	"Card3",
-	"Card4",
-	"Card5",
-	"Card6",
-	"Card7",
-	"Card8",
-	"Card9",
-	"Card10",
-	"Card11",
-	"Card12",
-	"Card13",
-	"Card14",
-	"Card15",
-	"Card16",
-	"Card17",
-	"Card18",
-	"Card19",
-	"Card20"
+	"https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/3_Tamagotchis.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/almost_beat_teen_pregnancy.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/Big_Dick_Energy.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/Dad_Bod..png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/Daddy_issues.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/I_sexually_identify_as_a_mistake.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/probably_drunk.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/questionable_sexual_orientation.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/several_online_degrees.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/silver_fox.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/too_emotionally_invested_in_my_plants_for_anything_serious_right_now.png",
+    "https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/VERY_flexible.png"
 ];
 
 var deck = new Array();
@@ -50,14 +42,16 @@ function renderDeck() {
 	document.getElementById('deck').innerHTML = '';
 	for(var i = 0; i < 3; i++) {
 
-        // edge case for 2 similar cards
-        if(deck[i].Card == deck[i+1].Card || deck[i].Card == deck[i+2].Card) deck[i].Card = deck[i+4].Card
+        // edge cases for 2 identical cards
+      	if(deck[i].Card == deck[i+1].Card || deck[i].Card == deck[i+2].Card) deck[i].Card = deck[i+4].Card;
+      	if(deck[i+1].Card == deck[i+2].Card) deck[i+1].Card = deck[i+3].Card;
 
+		var img = document.createElement("img");
 		var card = document.createElement("div");
-		var brand = '';
-		card.innerHTML = deck[i].Card + '' + brand;
-		card.classList.add('card');
-		document.getElementById("deck").appendChild(card);
+      	img.src = deck[i].Card;
+      	card.innerHTML = img;
+      	card.classList.add('card');
+      	document.getElementById("deck").appendChild(img);
 	}
 }
 
