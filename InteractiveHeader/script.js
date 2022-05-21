cf0f1f6823133a3c2a268a679a7fc09dbdfa4c3c
zayadur <zayadur@outlook.com>
2022-05-21 07:42:32 -0400

    Install Node for local testing, add basic tooltips, optimize script

--- InteractiveHeader/script.js
+++ InteractiveHeader/script.js
@@ -38,23 +38,24 @@ const cards = [
 	"https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/cards/313.png",
 	"https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/cards/319.png",
 	"https://raw.githubusercontent.com/zayadur/com.swipecardgame/main/InteractiveHeader/images/cards/328.png"
+
 ];
 
-let deck = [];
+let deck = []
 
 // add cards to deck
 function getDeck() {
 	for (let i = 0; i < cards.length; i++) {
-		let card = { Card: cards[i] };
-		deck.push(card);
+		let card = { Card: cards[i] }
+		deck.push(card)
 	}
-	return deck;
+	return deck
 }
 
 // target id="deck" and display 3 cards from deck
 function renderDeck() {
 
-	document.getElementById('deck').innerHTML = '';
+	document.getElementById('deck').innerHTML = ''	// resets render on shuffle
 
 	for (let i = 0; i < 3; i++) {
 
@@ -65,18 +66,13 @@ function renderDeck() {
 		 *		<img src=url class=card1>
 		 *		<img src=url class=card2>
 		 */
-		let img = document.createElement("img");
-		let card = document.createElement("div");
-		img.src = deck[i].Card;
-		card.innerHTML = img;
-		card.classList.add('card');
-		document.getElementById("deck").appendChild(img);
+		let img = document.createElement("img")
+		let card = document.createElement("div")
+		img.src = deck[i].Card
+		card.classList.add('card')
+		document.getElementById("deck").appendChild(img)
 
-		img.className += `card${i}`;	// add card[0-2] selectors to each card TEMPLATE LITERALS BABY!!!
-
-		// // edge cases for 2 identical cards
-		// if (deck[i].Card == deck[i + 1].Card || deck[i].Card == deck[i + 2].Card) deck[i].Card = deck[i + 4].Card;
-		// if (deck[i + 1].Card == deck[i + 2].Card) deck[i + 1].Card = deck[i + 3].Card;
+		img.className += `card${i}`	// add card[0-2] selectors to each card -- TEMPLATE LITERALS BABY!!!
 	}
 }
 
@@ -85,20 +81,40 @@ function renderDeck() {
  *	maybe there's a better way to do this
 */
 function shuffle() {
-	for (let i = 0; i < 36; i++) {
-		let position1 = Math.floor((Math.random() * deck.length));
-		let position2 = Math.floor((Math.random() * deck.length));
-		let tmp = deck[position1];
+	for (let i = 0; i < 38; i++) {
+		let position1 = Math.floor((Math.random() * deck.length))
+		let position2 = Math.floor((Math.random() * deck.length))
+		let tmp = deck[position1]
 
-		deck[position1] = deck[position2];
-		deck[position2] = tmp;
+		deck[position1] = deck[position2]
+		deck[position2] = tmp
 	}
-	renderDeck();
+	renderDeck()
+}
+
+function leftTip() {
+	document.getElementById("leftButton").addEventListener("click", () => {
+		document.getElementById("tooltip").innerHTML =
+			`
+				Oof! Better luck next time to that bozo!
+			`
+	})
+}
+
+function rightTip() {
+	document.getElementById("rightButton").addEventListener("click", () => {
+		document.getElementById("tooltip").innerHTML =
+			`
+				Nice! You just picked a winner!
+			`
+	})
 }
 
 function load() {
-	deck = getDeck();
-	shuffle();
+	deck = getDeck()
+	shuffle()
+	leftTip()	// not sure why I need to have this and rightTip on load, but it avoids requiring 2nd button press
+	rightTip()
 }
 
-window.addEventListener('load', load);
+window.addEventListener('load', load)
